32b602332f4eb97d6a455149824b3c3b1aef132a
Md Rashed Pervez <rpervez01@gmail.com>
2026-01-02 15:22:24 +0600

    Initial Commit

---
+++ News ExplorerUITests/News_ExplorerUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  News_ExplorerUITestsLaunchTests.swift
+//  News ExplorerUITests
+//
+//  Created by Rashed Pervez on 2/1/26.
+//
+
+import XCTest
+
+final class News_ExplorerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
